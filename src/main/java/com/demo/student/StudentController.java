4565ae39534c6e18e2b76cfe4fb9fb854a3b8430
Lesca-Ren <2017112119@ctgu.edu.cn>
2019-12-24 17:55:52 +0800

    student

---
+++ src/main/java/com/demo/student/StudentController.java
@@ -0,0 +1,79 @@
+package com.demo.student;
+
+import com.demo.common.model.Student;
+import com.jfinal.aop.Inject;
+import com.jfinal.core.Controller;
+import com.jfinal.json.Json;
+
+import java.util.List;
+
+public class StudentController extends Controller {
+    @Inject
+    StudentService studentService;
+
+    public void testPaper() {
+        render("testPaper.html");
+    }
+    public void practiceExam() {
+        render("practiceExam.html");
+    }
+    public void redo() {
+        render("redo.html");
+    }
+    public void viewResults() {
+        render("viewResults.html");
+    }
+    public void personalCenter() {
+        render("personalCenter.html");
+    }
+
+    public void index(){
+        setAttr("studentPage",studentService.paginate(getParaToInt(0, 1),10));
+        render("student.html");
+    }
+
+    public void list(){
+        int page = getParaToInt("page");
+        int limit = getParaToInt("limit");
+
+        List<Student> lists = studentService.paginate(page,limit).getList();
+        String jsonList = Json.getJson().toJson(lists);
+
+        int recordCount = studentService.allRecordCount();
+
+        String jsons= "{\"code\":\"0\",\"msg\":\"\",\"count\":\""+ recordCount +"\",\"data\":"+jsonList+"}";
+
+        renderJson(jsons);
+    }
+
+    public void delete(){
+        int id = Integer.parseInt(getPara("id"));
+        studentService.delete(id);
+        render("student.html");
+        //return 1;
+    }
+
+    public void update(){
+        int id = Integer.parseInt(getPara("id"));
+        String value = getPara("value");
+        String field = getPara("field");
+        studentService.update(id, field, value);
+    }
+
+    public void add(){
+        Student student = getModel(Student.class);
+        //System.out.println(student.get)
+        studentService.add(student);
+        redirect("/student");
+    }
+
+    public void practiceChinese(){
+        render("practicefile/practiceChinese.html");
+    }
+    public void practiceEnglish(){
+        render("practicefile/practiceEnglish.html");
+    }
+    public void practiceMath(){
+        render("practicefile/practiceMath.html");
+    }
+}
---
+++ src/main/java/com/demo/student/StudentService.java
@@ -0,0 +1,35 @@
+package com.demo.student;
+
+import com.demo.common.model.Student;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Page;
+import com.jfinal.plugin.activerecord.Record;
+
+
+public class StudentService {
+    private Student dao = new Student().dao();
+
+    public Page<Student> paginate(int pageNumber, int pageSize){
+        return dao.paginate(pageNumber,pageSize,"select *","from student order by id asc");
+    }
+
+    int allRecordCount(){
+        return Db.queryInt("select count(*) from student");
+    }
+
+    void delete(int id){
+        dao.deleteById(id);
+    }
+
+    public void update(int id, String field,String value){
+        Record student = new Record();
+        student = Db.findById("student",id).set(field,value);
+        Db.update("student",student);
+    }
+
+    public void add(Student student1){
+        Record student = new Record().set("class_id",student1.getClassId())
+                .set("userlv",student1.getUserlv()).set("user_id",student1.getUserId());
+        Db.save("student",student);
+    }
+}
