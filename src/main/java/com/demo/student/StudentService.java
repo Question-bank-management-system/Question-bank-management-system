07d7b89a62a41ab00b690283ec89a2419eb447cd
ghgjhgjj <3441884848@qq.com>
2019-12-25 16:37:29 +0800

    修复

--- src/main/java/com/demo/student/StudentService.java
+++ src/main/java/com/demo/student/StudentService.java
@@ -27,9 +27,10 @@ public class StudentService {
         Db.update("student",student);
     }
 
-//    public void add(Student student1){
-//        Record student = new Record().set("class_id",student1.getClassId())
-//                .set("user_id",student1.getUserId());
-//        Db.save("student",student);
-//    }
+    public void add(Student student1){
+        Record student = new Record().set("user_id",student1.getUserId())
+                .set("test_paperid",student1.getTestPaperid())
+                .set("classid",student1.getClassid()).set("score",student1.getScore());
+        Db.save("student",student);
+    }
 }
