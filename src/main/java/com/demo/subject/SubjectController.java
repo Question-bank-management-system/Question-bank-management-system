81d6c6e17d126d14f186737e2a4c719a1a680747
ghgjhgjj <3441884848@qq.com>
2019-12-25 17:34:56 +0800

    完善democonfig

---
+++ src/main/java/com/demo/subject/SubjectController.java
@@ -0,0 +1,54 @@
+package com.demo.subject;
+
+import com.demo.subject.SubjectService;
+import com.demo.common.model.Subject;
+import com.jfinal.aop.Inject;
+import com.jfinal.core.Controller;
+import com.jfinal.json.Json;
+
+import java.util.List;
+
+public class SubjectController extends Controller {
+    @Inject
+    SubjectService subjectService;
+
+    public void index(){
+        //setAttr("adminUserPage",adminService.paginate(getParaToInt(0, 1),10));
+        render("subject.html");
+    }
+
+    public void list(){
+        int page = getParaToInt("page");
+        int limit = getParaToInt("limit");
+
+        List<Subject> lists = subjectService.paginate(page,limit).getList();
+        String jsonList = Json.getJson().toJson(lists);
+
+        int recordCount = subjectService.allRecordCount();
+
+        String jsons= "{\"code\":\"0\",\"msg\":\"\",\"count\":\""+ recordCount +"\",\"data\":"+jsonList+"}";
+
+        renderJson(jsons);
+    }
+
+    public void delete(){
+        int id = Integer.parseInt(getPara("id"));
+        subjectService.delete(id);
+        render("subject.html");
+        //return 1;
+    }
+
+    public void update(){
+        int id = Integer.parseInt(getPara("id"));
+        String value = getPara("value");
+        String field = getPara("field");
+        subjectService.update(id, field, value);
+    }
+
+    public void add(){
+        Subject subject = getModel(Subject.class);
+        //System.out.println(student.get)
+        subjectService.add(subject);
+        redirect("/subject");
+    }
+}
---
+++ src/main/java/com/demo/tempPaper/TempPaperController.java
@@ -0,0 +1,54 @@
+package com.demo.tempPaper;
+
+import com.demo.common.model.TempPaper;
+import com.demo.tempPaper.TempPaperService;
+import com.jfinal.aop.Inject;
+import com.jfinal.core.Controller;
+import com.jfinal.json.Json;
+
+import java.util.List;
+
+public class TempPaperController extends Controller {
+    @Inject
+    TempPaperService tempPaperService;
+
+    public void index(){
+        //setAttr("adminUserPage",adminService.paginate(getParaToInt(0, 1),10));
+        render("tempPaper.html");
+    }
+
+    public void list(){
+        int page = getParaToInt("page");
+        int limit = getParaToInt("limit");
+
+        List<TempPaper> lists = tempPaperService.paginate(page,limit).getList();
+        String jsonList = Json.getJson().toJson(lists);
+
+        int recordCount = tempPaperService.allRecordCount();
+
+        String jsons= "{\"code\":\"0\",\"msg\":\"\",\"count\":\""+ recordCount +"\",\"data\":"+jsonList+"}";
+
+        renderJson(jsons);
+    }
+
+    public void delete(){
+        int id = Integer.parseInt(getPara("id"));
+        tempPaperService.delete(id);
+        render("tempPaper.html");
+        //return 1;
+    }
+
+    public void update(){
+        int id = Integer.parseInt(getPara("id"));
+        String value = getPara("value");
+        String field = getPara("field");
+        tempPaperService.update(id, field, value);
+    }
+
+    public void add(){
+        TempPaper tempPaper = getModel(TempPaper.class);
+        //System.out.println(student.get)
+        tempPaperService.add(tempPaper);
+        redirect("/tempPaper");
+    }
+}
