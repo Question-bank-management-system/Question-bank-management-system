5004c886373f0bb686c42e37048116ac6deda7d7
Lesca-Ren <2017112119@ctgu.edu.cn>
2019-12-25 13:28:18 +0800

    个人中心页面修改以及数据库版本更新

--- 启动说明/online_judge_five.sql
+++ 启动说明/online_judge_five.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100408
  File Encoding         : 65001
 
- Date: 25/12/2019 11:43:30
+ Date: 25/12/2019 13:27:18
 */
 
 SET NAMES utf8mb4;
@@ -58,14 +58,28 @@ CREATE TABLE `class`  (
 DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `class_id` int(11) NULL DEFAULT NULL,
   `user_id` int(11) NOT NULL,
+  `test_paperid` int(11) NULL DEFAULT NULL,
+  `classid` int(11) NULL DEFAULT NULL,
+  `score` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `fk_user_student_id`(`user_id`) USING BTREE,
-  INDEX `fk_class_student_id`(`class_id`) USING BTREE,
-  CONSTRAINT `fk_class_student_id` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_user_student_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  INDEX `fk_test_paper_student_id`(`test_paperid`) USING BTREE,
+  INDEX `fk_class_student_id`(`classid`) USING BTREE,
+  CONSTRAINT `fk_user_student_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_class_student_id` FOREIGN KEY (`classid`) REFERENCES `class` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_test_paper_student_id` FOREIGN KEY (`test_paperid`) REFERENCES `test_paper` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for subject
+-- ----------------------------
+DROP TABLE IF EXISTS `subject`;
+CREATE TABLE `subject`  (
+  `subject_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for teacher
@@ -90,8 +104,10 @@ CREATE TABLE `test_paper`  (
   `starttime` datetime(0) NOT NULL,
   `endtime` datetime(0) NOT NULL,
   `full_marks` int(11) NULL DEFAULT NULL,
-  `subject_id` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+  `classid` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_class_test_paper`(`classid`) USING BTREE,
+  CONSTRAINT `fk_class_test_paper` FOREIGN KEY (`classid`) REFERENCES `class` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
@@ -122,6 +138,7 @@ CREATE TABLE `user`  (
   `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `birthdate` date NULL DEFAULT NULL,
+  `sex` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
